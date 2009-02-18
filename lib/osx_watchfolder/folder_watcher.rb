7529bcb0eb603954e454263ec413f1ad1d055f02
Paul Wilson <paul.wilson@merecomplexities.com>
2009-02-18 16:19:19 +0000

    Cleans up and handles interrupts

--- lib/osx_watchfolder/folder_watcher.rb
+++ lib/osx_watchfolder/folder_watcher.rb
@@ -3,38 +3,38 @@ OSX.require_framework '/System/Library/Frameworks/CoreServices.framework/Framewo
 
 module OsxWatchfolder
 
+  # To run a the method 'run_tests' when a change is detected in a couple of folders:
+  #
+  #  folder_watcher = OsxWatchfolder::FolderWatcher.new("lib", "test") { run_tests}
+  #  folder_wacher.start
+  #
   class FolderWatcher
-
-    attr_accessor :latency, :runloop_interval
+    
+    # directory update notification latency in seconds.  Defaults to 1 
+    attr_accessor :latency
+    
+    # runloop timeout in seconds - on all but the 1st interrupt for the script it will take a maximum of 
+    # this long for interrupts to be noticed. Defaults to 5
+    attr_accessor :runloop_interval
+    
+    # how many times has this been interrupted?
+    attr_reader :interrupted_count
 
 
     def stop
+      @running = false
       OSX::CFRunLoopStop(@runloop)
     end
 
     def start
       raise "May only be started from main thread" unless Thread.current == Thread.main
-      callback = lambda do |streamRef, clientCallBackInfo, numEvents,eventPaths, eventFlags,eventIds| 
-        @block.call
-      end
-      stream = OSX::FSEventStreamCreate(nil,
-      callback,
-      nil,
-      @folders,
-      OSX::KFSEventStreamEventIdSinceNow, 
-      @latency,
-      OSX::KFSEventStreamCreateFlagNone)
-
-      @runloop = OSX::CFRunLoopGetCurrent()
-
-      OSX::FSEventStreamScheduleWithRunLoop(stream, @runloop, OSX::KCFRunLoopDefaultMode)   
-      OSX::FSEventStreamStart(stream)
-      
-      OSX::CFRunLoopRun()
-      
-      OSX::FSEventStreamStop(stream)
-      OSX::FSEventStreamInvalidate(stream)
-      OSX::FSEventStreamRelease(stream)
+      prepare_stream
+      enter_run_loop
+    rescue Interrupt
+      stop
+      @interrupted_count += 1
+    ensure
+      cleanup_stream 
     end  
 
 
@@ -43,7 +43,40 @@ module OsxWatchfolder
       @block = block
       @running = true
       @latency = 1
+      @runloop_interval = 5
+      @interrupted_count = 0
     end
+
+    private
+
+
+    def enter_run_loop
+      @running = true
+      OSX::CFRunLoopRunInMode(OSX::KCFRunLoopDefaultMode, @runloop_interval, true) while @running
+    end
+
+    def cleanup_stream 
+      if (@stream)
+        OSX::FSEventStreamStop(@stream)
+        OSX::FSEventStreamInvalidate(@stream)
+        OSX::FSEventStreamRelease(@stream)
+      end
+      @stream = nil
+    end
+
+    def prepare_stream
+      callback = lambda do |streamRef, clientCallBackInfo, numEvents,eventPaths, eventFlags,eventIds| 
+        @block.call
+      end
+      @stream = OSX::FSEventStreamCreate(nil,callback,nil,@folders,OSX::KFSEventStreamEventIdSinceNow, @latency,OSX::KFSEventStreamCreateFlagNone)
+
+      @runloop = OSX::CFRunLoopGetCurrent()
+
+      OSX::FSEventStreamScheduleWithRunLoop(@stream, @runloop, OSX::KCFRunLoopDefaultMode)   
+      OSX::FSEventStreamStart(@stream)
+    end
+
+
 
   end
 end
--- test/test_folder_watch.rb
+++ test/test_folder_watch.rb
@@ -4,8 +4,8 @@ require 'fileutils'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/osx_watchfolder')
 
 class TestFolderWatch <  Test::Unit::TestCase
-include FileUtils
-include OsxWatchfolder
+  include FileUtils
+  include OsxWatchfolder
 
   def setup
     @some_folders = [Dir.tmpdir + "/folder_watch_test1", Dir.tmpdir + "/folder_watch_test2"]
@@ -13,11 +13,11 @@ include OsxWatchfolder
     @some_folders.each {|dir| mkdir dir}
     @folder_changed = false
   end
-  
+
   def teardown
     @some_folders.each {|dir| rm_rf dir}
   end
-  
+
   def test_nothing_happens_if_a_watched_folder_does_not_change
     in_a_second_yield_and_stop_watcher
     watch_folders
@@ -29,20 +29,20 @@ include OsxWatchfolder
     assert_equal @some_folders,  FolderWatcher.new(@some_folders).instance_variable_get(:@folders)
   end
 
-  
+
   def test_notified_if_first_watched_folder_changes
     in_a_second_write_file_and_stop_watcher @some_folders.first + "/somefile"
     watch_folders
     assert @folder_changed    
   end
-  
+
 
   def test_notified_if_another_watched_folder_changes
     in_a_second_write_file_and_stop_watcher @some_folders.last + "/somefile"
     watch_folders
     assert @folder_changed    
   end
- 
+
   def test_must_be_started_in_main_thread
     @testee = FolderWatcher.new(*@some_folders) {}
     t = Thread.new do
@@ -50,8 +50,20 @@ include OsxWatchfolder
     end
     t.join
   end
- 
- 
+
+  def test_handles_interrupts
+    @testee = FolderWatcher.new(*@some_folders) {}
+    assert_equal 0, @testee.interrupted_count 
+    class << @testee
+      def enter_run_loop
+        raise Interrupt, "stopped!"
+      end
+    end
+    @testee.start
+    assert_equal 1, @testee.interrupted_count 
+  end
+  
+
   def in_a_second_yield_and_stop_watcher
     Thread.new do
       yield if block_given?
@@ -59,19 +71,19 @@ include OsxWatchfolder
       @testee.stop
     end
   end
- 
+
   def in_a_second_write_file_and_stop_watcher(file)
     in_a_second_yield_and_stop_watcher do
       File.open(file, "w"){|f| f.write "hello"}
     end
   end
-  
+
   def watch_folders
     @testee = FolderWatcher.new(*@some_folders) {@folder_changed = true}
     @testee.latency = 0.1
     @testee.runloop_interval = 0.5
     @testee.start
   end
-    
-  
+
+
 end
